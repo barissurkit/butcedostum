283a74a610a1b41cae3aca9c167b647ae9454202
Lokman Efe <65876412+lokicik@users.noreply.github.com>
2025-12-18 05:43:17 +0300

    feat: migrate to neondb

--- next.config.js
+++ next.config.js
@@ -1,31 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {},
-  serverExternalPackages: [
-    "@prisma/adapter-libsql",
-    "@libsql/client",
-    "@libsql/hrana-client",
-    "@libsql/isomorphic-ws",
-    "@libsql/win32-x64-msvc",
-  ],
-
-  webpack(config, { isServer }) {
-    config.module.rules.push({
-      test: /\.(md|txt)$/i,
-      type: "asset/source",
-    });
-
-    config.module.rules.push({
-      test: /(LICENSE|NOTICE|COPYING)$/i,
-      type: "asset/source",
-    });
-
-    if (isServer) {
-      config.externals.push("@libsql/win32-x64-msvc");
-    }
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
