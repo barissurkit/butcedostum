787108d2de810e27a952a5c9d13052880c171c6b
Barış Sürkit <bbarisrkt@gmail.com>
2025-12-23 14:04:32 +0300

    feat: page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function HomePage() {
         <h1 className="h1">BütçeDostum</h1>
         <p className="muted">Gelir-gider takibiniz kısacası "Bütçeniz" için bir dost.</p>
         <a href="/login">Giriş Yap</a>
+        <br /> <hr />
         <a href="/register">Kayıt Ol</a>
       </div>
     </main>
