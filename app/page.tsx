76c784ec7dd197a56f097b3b3872569283b04fe5
Barış Sürkit <bbarisrkt@gmail.com>
2025-12-23 14:06:26 +0300

    feat: ...

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function HomePage() {
         <p className="muted">Gelir-gider takibiniz kısacası "Bütçeniz" için bir dost.</p>
         <a href="/login">Giriş Yap</a>
         <br /> <hr />
+        <a href="/register">Kayıt Ol</a>
       </div>
     </main>
   );
